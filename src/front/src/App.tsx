95825f73ed0324a481c57808eb8e10d154fea8d9
Flavio Ribeiro <flavioribeirojr@outlook.com>
2022-09-18 14:24:33 -0300

    Add front project

---
+++ src/front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <Button variant="contained">
+      Hello world!
+    </Button>
+  );
+}
+
+export default App;
